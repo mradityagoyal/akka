94d0a13b24f1d8d8eb736c7e2283804a92be3cee
Aditya Goyal <Aditya.G.Goyal@oracle.com>
2016-09-06 13:43:56 -0400

    stuff

--- akkaStreams/src/main/java/com/mradityagoyal/akka/streams/kafka/KafkaProducerDemo.java
+++ akkaStreams/src/main/java/com/mradityagoyal/akka/streams/kafka/KafkaProducerDemo.java
@@ -1,12 +1,14 @@
 package com.mradityagoyal.akka.streams.kafka;
 
 import java.util.Arrays;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import akka.Done;
 import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.kafka.ProducerSettings;
@@ -15,6 +17,7 @@ import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
 import akka.stream.ThrottleMode;
 import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.Keep;
 import akka.stream.javadsl.RunnableGraph;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
@@ -22,40 +25,68 @@ import scala.concurrent.duration.Duration;
 
 public class KafkaProducerDemo {
 	final ActorSystem system;
-	final Materializer materializer ;
+	final Materializer materializer;
 	protected final ProducerSettings<byte[], String> producerSettings;
-	
-	
+
 	public KafkaProducerDemo() {
 		super();
-		system = ActorSystem.create("QuickStart"); //TODO: configure system name. 
+		system = ActorSystem.create("QuickStart"); // TODO: configure system
+													// name.
 		materializer = ActorMaterializer.create(system);
-		producerSettings = ProducerSettings
-				  .create(system, new ByteArraySerializer(), new StringSerializer())
-				  .withBootstrapServers("localhost:9092"); //TODO: configure kafka port.  
+		producerSettings = ProducerSettings.create(system, new ByteArraySerializer(), new StringSerializer())
+				.withBootstrapServers("localhost:9092"); // TODO: configure
+															// kafka port.
 	}
 
+	public static void main(String[] args) {
 
-	public static void main(String[] args){
-		
 		KafkaProducerDemo demo = new KafkaProducerDemo();
-		String msg = "irst we use the scan combinator to run a computation over the whole stream: starting with the number 1 (BigInteger.ONE) we multiple by each of the incoming numbers, one after the other; the scan operationemits the initial value and then every calculation result. This yields the series of factorial numbers which we stash away as a Source for later reuse—it is important to";
+		String msg = "what's goig on .. ha ha. ha.  ";
 		Source<String, NotUsed> source = Source.from(Arrays.asList(msg.split(" ")));
-		RunnableGraph<NotUsed> runnableGraph = source.throttle(1, Duration.create(1, TimeUnit.SECONDS), 1, ThrottleMode.shaping()).to(demo.mapToProducerRecordAndSend("topic2"));
-		runnableGraph.run(demo.materializer);
+		Source<ProducerRecord<byte[], String>, NotUsed> producerRecords = source
+				.throttle(1, Duration.create(1, TimeUnit.SECONDS), 1, ThrottleMode.shaping())
+				.map(s -> new ProducerRecord<>("topic2", s));
+
+		final RunnableGraph<CompletionStage<Done>> graph = producerRecords.toMat(Producer.plainSink(demo.producerSettings), Keep.right());
+		final CompletionStage<Done> done = producerRecords.runWith(Producer.plainSink(demo.producerSettings),
+				demo.materializer);
+		done.thenRun(() -> {
+			System.out.println("Completed");
+			
+		});
 		
-		
-		
+		final CompletionStage<Done> done2 = graph.run(demo.materializer);
+		done2.thenRun(() -> System.out.println("second is done"));
+
+		// RunnableGraph<NotUsed> runnableGraph = source.throttle(1,
+		// Duration.create(1, TimeUnit.SECONDS), 1,
+		// ThrottleMode.shaping()).to(demo.mapToProducerRecordAndSend("topic2"));
+		// NotUsed nu = runnableGraph.run(demo.materializer);
+		// System.out.println(nu);
+
 	}
-	
+
 	/**
-	 * returns a Sink that consumes elements from a String Source to kafka topic. 
+	 * returns a Sink that consumes elements from a String Source to kafka
+	 * topic.
+	 * 
 	 * @param topic
-	 * @return Sink<String, NotUsed> This sink can be used to send elements from a String Source to kafka topic. 
+	 * @return Sink<String, NotUsed> This sink can be used to send elements from
+	 *         a String Source to kafka topic.
 	 */
-	public Sink<String, NotUsed> mapToProducerRecordAndSend(String topic){
-		 return Flow.of(String.class).map(s -> new ProducerRecord<byte[], String>(topic, s)).to(Producer.plainSink(producerSettings));
-				
+	public Sink<String, NotUsed> mapToProducerRecordAndSend(String topic) {
+		return Flow.of(String.class).map(s -> new ProducerRecord<byte[], String>(topic, s))
+				.to(Producer.plainSink(producerSettings));
+
 	}
+
+	// public Sink<ProducerRecord<byte[], String>, CompletionStage<Done>>
+	// getCompletableSink(String topic){
+	// final Sink<ProducerRecord<byte[], String>, CompletionStage<Done>> sink1 =
+	// Producer.plainSink(producerSettings);
+	// final Flow.
+	//// return sink1;
+	//
+	// }
 
 }
